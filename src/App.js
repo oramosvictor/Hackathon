80d5c7365ab7d72cc96b5af9d54c3afa66eff5d8
oramosvictor <victor10rsilva@gmail.com>
2022-11-26 13:24:30 -0300

    projeto react - Victor Ramos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/js/bootstrap";
+import React, { useState, useEffect } from "react";
+import Card from "./components/Card/Card";
+import Search from "./components/Search/Search"
+import Pagination from "./components/Pagination/Pagination"
 
 function App() {
+
+  let [fetchedData, updateFetchedData] = useState([]);
+  let { info, results } = fetchedData;
+  let [pageNumber, updatePageNumber] = useState(1);
+  let [search, setSearch] = useState("");
+
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${search}`;
+  useEffect(() => {
+    (async function () {
+      let data = await fetch(api).then((res) => res.json());
+      updateFetchedData(data);
+    })();
+  }, [api]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1 className="text-center mb-3">Personagens</h1>
+
+    <Search setSearch={setSearch} updatePageNumber={updatePageNumber} />
+
+    <div className="container">
+    <div className="row">
+
+      <div className="center-col-6">       
+            <div className="row">
+              <Card page="/" results={results} />
+            </div>         
+      </div>
     </div>
+    </div>
+
+    <Pagination
+        info={info}
+        pageNumber={pageNumber}
+        updatePageNumber={updatePageNumber}
+      />
+  </div>
   );
 }
 
---
+++ src/components/Card/Card.js
@@ -0,0 +1,68 @@
+import React from "react";
+import styles from "./Card.module.scss";
+
+
+const Card = ({ page, results }) => {
+
+    let display;
+
+    if (results) {
+        display = results.map((x) => {
+        let { id, image, name, status, location } = x;
+
+          return (
+            <div
+            key={id}
+            className="col-lg-4 col-md-6 col-sm-6 col-12 mb-4 position-relative text-dark"
+          >
+            <div className={`${styles.card} d-flex flex-column justify-content-center`}>
+                <img className={`${styles.img} img-fluid`} src={image} alt="" />
+                <div className={`${styles.content}`}>
+                    <div className="fs-5 fw-bold mb-4">{name}</div>
+                    <div className="">
+                        <div className="fs-6 fw-normal">Last Location</div>
+                        <div className="fs-5">{location.name}</div>
+                    </div>
+                </div>
+            </div>
+
+            {(() => {
+            if (status === "Dead") {
+              return (
+                <div
+                  className={`${styles.badge} position-absolute badge bg-danger`}
+                >
+                  {status}
+                </div>
+              );
+            } else if (status === "Alive") {
+              return (
+                <div
+                  className={`${styles.badge} position-absolute badge bg-success`}
+                >
+                  {status}
+                </div>
+              );
+            } else {
+              return (
+                <div
+                  className={`${styles.badge} position-absolute badge bg-secondary`}
+                >
+                  {status}
+                </div>
+              );
+            }
+          })()}
+
+            </div>
+        );
+      });
+      }
+    else{
+      display = "Nenhum personagem encontrado :/";
+    }
+
+    return <>{display}</>;
+  }
+
+  export default Card;
